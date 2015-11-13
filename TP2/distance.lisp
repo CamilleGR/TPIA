58e7625f4ad2ea2d115e75a3eae624a450694a0e
CamilleGR <gerinrozecamille@gmail.com>
2015-11-13 09:59:34 +0100

    NO CHANGE

--- TP2/distance.lisp
+++ TP2/distance.lisp
@@ -11,7 +11,6 @@
   (let ((dist 0))
     (do ((x etatA (cdr x))(y etatB (cdr y)))
 	((AND (NULL x) (NULL y)) dist)
-     ;;;; (format t "~a et ~a~%" x y)
       (if (NOT (EQUAL (car x) (car y)))
 	  (setq dist (1+ dist))
 	)
@@ -30,8 +29,6 @@
     (dolist (e liste minEtat)
       (if (<= (distance etat e) minDist)
 	  (progn
-	    (print minDist)
-	    (setq minDist (distance e etat))
 	    (push e minEtat)
 	    )
 	)
