4de710f880312d5cb9907ec9146f14b93ef0cae7
CamilleGR <gerinrozecamille@gmail.com>
2015-11-14 21:19:59 +0100

    Gestion des mauvaises entrées

--- TP2/TP2.lisp
+++ TP2/TP2.lisp
@@ -193,6 +193,9 @@
 
 
 (DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
+  (if (NOT (AND (etat_correct etatCourrant) (etat_correct etatFinal)))
+      (error "ERREUR : ENTREE(S) INCORRECT(S)")
+      )
   (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
     (format t "~T"))                        ;;;; On affiche une tabulation 
   (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
