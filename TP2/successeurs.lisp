91e4ca639463ffb516f9be24dd69168cd520dca7
Thomas Perrin <thomas.lb.perrin@gmail.com>
2015-11-14 16:20:25 +0100

    Update with loop for

--- TP2/successeurs.lisp
+++ TP2/successeurs.lisp
@@ -1,12 +1,20 @@
 (load "echange.lisp")
 (load "etat-correct.lisp")
 
-(defun successeurs (etat)
+#|(defun successeurs (etat)
   (let ((value ()))
   (dotimes (i 4 value)
     (if (NOT (= i 1))
 	(if (etat_correct (echange etat (+ i 1) 2))
 	    (push (echange etat (+ i 1) 2) value)))))
+)|#
+
+(defun successeurs (etat)
+  (let ((value ()))
+  (loop for i in (2 3 4)
+    do (if (etat_correct (echange etat (+ i 1) 2))
+	    (push (echange etat (+ i 1) 2) value)))
+	value)
 )
 
 
