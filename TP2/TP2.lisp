dc892411d28205238d4d0874c66d1f8b256547cb
Thomas Perrin <thomas.lb.perrin@gmail.com>
2015-11-15 19:17:06 +0100

    MAJ fonction recherche_opti

--- TP2/TP2.lisp
+++ TP2/TP2.lisp
@@ -217,7 +217,7 @@
   )
 
 
-(DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
+#|(DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
   (if (NOT (AND (etat_correct etatCourrant) (etat_correct etatFinal)))
       (error "ERREUR : ENTREE(S) INCORRECT(S)")
       )
@@ -232,4 +232,28 @@
 	)
       )
       )
+  )|#
+  
+  (DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
+  (if (NOT (AND (etat_correct etatCourrant) (etat_correct etatFinal)))
+      (error "ERREUR : ENTREE(S) INCORRECT(S)")
+      )
+  (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
+    (format t "~T"))                        ;;;; On affiche une tabulation 
+  (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
+  (IF (EQUAL etatCourrant etatFinal)
+      (append etatsParcourus (list etatCourrant)) ;;;; Si l'état courrant est l'état final, alors on a trouvé le chemin
+	  (let ((etatPrometteur (choixEtat etatFinal (successeurs etatCourrant) etatsParcourus))) ;;;; On choisi l'état le plus prometteur
+      	(loop while (not (null etatPrometteur))
+			do (if (NOT (MYMEMBER etatPrometteur etatsParcourus)) ;;;; Si l'état le plus prometteur n'a pas déjà été parcouru on continu notre exploration
+			  (SETQ sol (recherche_opti etatPrometteur etatFinal (append etatsParcourus (list etatCourrant))))
+			)
+			(if (null sol)
+				(setq etatPrometteur (choixEtat etatFinal (remove etatPrometteur (successeurs etatCourrant)) etatsParcourus))
+				(setq etatPrometteur NIL)
+			)
+		 )
+    sol)
   )
+ )
+ 
