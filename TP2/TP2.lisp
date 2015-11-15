5e9ba3d32c50bb7d408e225feb25e3200930dddb
CamilleGR <gerinrozecamille@gmail.com>
2015-11-15 17:40:48 +0100

    Ajout de tests unitaires et de la fonction recherche_opti_liste

--- TP2/TP2.lisp
+++ TP2/TP2.lisp
@@ -181,6 +181,19 @@
     )
   )
 
+(defun plusProche (etat liste)
+  (let ((minDist (distance etat (car liste)))(minEtat (list (car liste))))
+    (dolist (e liste minEtat)
+      (cond ((= (distance etat e) minDist)
+	     (push e minEtat)
+	     )
+	    ((<  (distance etat e) minDist)
+	     (setq minDist (distance etat e))
+	     (setq minEtat (list e)))
+	    )
+      )
+    )
+  )
 
 ;;;; --------------------------------------------------------------------
 ;;;;                        RECHERCHE_OPTI
@@ -189,6 +202,23 @@
 ;;;; fois l'état le plus proche de notre état d'arrivée.
 ;;;;
 ;;;; ---------------------------------------------------------------------
+
+(DEFUN recherche_opti_liste ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
+  (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
+    (format t "~T"))                        ;;;; On affiche une tabulation 
+  (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
+  (IF (EQUAL etatCourrant etatFinal)
+      (append etatsParcourus (list etatCourrant)) ;;;; Si l'état courrant est l'état final, alors on a trouvé le chemin 
+      (LET ((list-succ (plusProche etatFinal (successeurs etatCourrant))) (sol nil)) ;;;; Déclaration de nos variables
+	(DO ((succ (pop list-succ) (pop list-succ))) ;;;; succ va prendre la valeur de chaque successeur possible
+	    ((OR (null succ) (not (null sol))) sol) ;;;; On continue tant qu'il y a des successeurs possibles et que nous n'avons pas de solutions
+	  (IF (AND (NOT (myMember succ etatsParcourus)) (not (null succ))) ;;;; si l'état n'a pas déjà été parcouru
+	      (SETQ sol (recherche_opti_liste succ etatFinal (append etatsParcourus (list etatCourrant)))) ;;;; on rappelle notre fonction recherche
+	      )
+	  )
+	)
+      )
+  )
 
 
 (DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
---
+++ TP2/test.lisp
@@ -0,0 +1,25 @@
+(load "TP2.lisp")
+
+
+(defparameter *etatsPossibles* '((A B C D) (A B D C) (A C B D)
+				 (A C D B) (A D C B) (B A C D)
+				 (B A D C) (B C A D) (C A B D)
+				 (C A D B) (C B A D) (A D B C)))
+
+(defun test_recherche_opti ()
+  (dolist (e *etatsPossibles* NIL)
+    (dolist (et *etatsPossibles* NIL)
+      (format t "~% ~a et ~a ~%" e et)
+      (format t "~a~%" (recherche_opti e et))
+      )
+    )
+  )
+
+(defun test_recherche_opti_liste ()
+  (dolist (e *etatsPossibles* 'OK)
+    (dolist (et *etatsPossibles* 'OK)
+      (format t "~% ~a et ~a ~%" e et)
+      (format t "~a~%" (recherche_opti_liste e et))
+      )
+    )
+  )
