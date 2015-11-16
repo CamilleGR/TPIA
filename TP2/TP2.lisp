59888a5d1db53227b7328b626e2467d1bdaf6062
CamilleGR <gerinrozecamille@gmail.com>
2015-11-16 02:19:43 +0100

    Cleanning

--- TP2/TP2.lisp
+++ TP2/TP2.lisp
@@ -216,22 +216,6 @@
   )
 
 
-#|(DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
-  (if (NOT (AND (etat_correct etatCourrant) (etat_correct etatFinal)))
-      (error "ERREUR : ENTREE(S) INCORRECT(S)")
-      )
-  (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
-    (format t "~T"))                        ;;;; On affiche une tabulation 
-  (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
-  (IF (EQUAL etatCourrant etatFinal)
-      (append etatsParcourus (list etatCourrant)) ;;;; Si l'état courrant est l'état final, alors on a trouvé le chemin
-      (let ((etatPrometteur (choixEtat etatFinal (successeurs etatCourrant) etatsParcourus))) ;;;; On choisi l'état le plus prometteur
-      	(if (NOT (MYMEMBER etatPrometteur etatsParcourus)) ;;;; Si l'état le plus prometteur n'a pas déjà été parcouru on continu notre exploration
-		  (SETQ sol (recherche_opti etatPrometteur etatFinal (append etatsParcourus (list etatCourrant))))
-	)
-      )
-      )
-  )|#
   
   (DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
   (if (NOT (AND (etat_correct etatCourrant) (etat_correct etatFinal)))
