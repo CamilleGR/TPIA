50cb8ee02df1d6155f151e07667751458455d521
Thomas Perrin <thomas.lb.perrin@gmail.com>
2015-11-16 18:44:47 +0100

    Update TP2.lisp

--- TP2/TP2.lisp
+++ TP2/TP2.lisp
@@ -111,9 +111,6 @@
 
 
 (DEFUN recherche ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
-  (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
-    (format t "~T"))                        ;;;; On affiche une tabulation 
-  (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
   (IF (EQUAL etatCourrant etatFinal)
       (append etatsParcourus (list etatCourrant)) ;;;; Si l'état courrant est l'état final, alors on a trouvé le chemin 
       (LET ((list-succ (successeurs etatCourrant)) (sol nil)) ;;;; Déclaration de nos variables
@@ -179,20 +176,6 @@
     )
   )
 
-(defun plusProche (etat liste)
-  (let ((minDist (distance etat (car liste)))(minEtat (list (car liste))))
-    (dolist (e liste minEtat)
-      (cond ((= (distance etat e) minDist)
-	     (push e minEtat)
-	     )
-	    ((<  (distance etat e) minDist)
-	     (setq minDist (distance etat e))
-	     (setq minEtat (list e)))
-	    )
-      )
-    )
-  )
-
 ;;;; --------------------------------------------------------------------
 ;;;;                        RECHERCHE_OPTI
 ;;;;
@@ -201,46 +184,25 @@
 ;;;;
 ;;;; ---------------------------------------------------------------------
 
-(DEFUN recherche_opti_liste ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
-  (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
-    (format t "~T"))                        ;;;; On affiche une tabulation 
-  (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
-  (IF (EQUAL etatCourrant etatFinal)
-      (append etatsParcourus (list etatCourrant)) ;;;; Si l'état courrant est l'état final, alors on a trouvé le chemin 
-      (LET ((list-succ (plusProche etatFinal (successeurs etatCourrant))) (sol nil)) ;;;; Déclaration de nos variables
-	(DO ((succ (pop list-succ) (pop list-succ))) ;;;; succ va prendre la valeur de chaque successeur possible
-	    ((OR (null succ) (not (null sol))) sol) ;;;; On continue tant qu'il y a des successeurs possibles et que nous n'avons pas de solutions
-	  (IF (AND (NOT (myMember succ etatsParcourus)) (not (null succ))) ;;;; si l'état n'a pas déjà été parcouru
-	      (SETQ sol (recherche_opti_liste succ etatFinal (append etatsParcourus (list etatCourrant)))) ;;;; on rappelle notre fonction recherche
-	      )
-	  )
-	)
-      )
-  )
-
-
   
   (DEFUN recherche_opti ( etatCourrant etatFinal &optional etatsParcourus) ;;;; etatsParcourus est optionnel
   (if (NOT (AND (etat_correct etatCourrant) (etat_correct etatFinal)))
       (error "ERREUR : ENTREE(S) INCORRECT(S)")
       )
-  (dotimes (i (list-length etatsParcourus)) ;;;; On fait la boucle autant de fois qu'il y'a détats parcourus
-    (format t "~T"))                        ;;;; On affiche une tabulation 
-  (format t "~A~%" etatCourrant)            ;;;; On affiche en suite l'état courrant
   (IF (EQUAL etatCourrant etatFinal)
       (append etatsParcourus (list etatCourrant)) ;;;; Si l'état courrant est l'état final, alors on a trouvé le chemin
 	  (let ((etatPrometteur (choixEtat etatFinal (successeurs etatCourrant) etatsParcourus))) ;;;; On choisi l'état le plus prometteur
-      	(loop while (not (null etatPrometteur))
-	      do ;;;; Si l'état le plus prometteur n'a pas déjà été parcouru on continu notre exploration
-	      (if (NOT (MYMEMBER etatPrometteur etatsParcourus)) 
-			  (SETQ sol (recherche_opti etatPrometteur etatFinal (append etatsParcourus (list etatCourrant))))
-			)
-			(if (null sol)
-				(setq etatPrometteur (choixEtat etatFinal (remove etatPrometteur (successeurs etatCourrant)) etatsParcourus))
-				(setq etatPrometteur NIL)
-			)
+	      	(loop while (not (null etatPrometteur))
+		      do ;;;; Si l'état le plus prometteur n'a pas déjà été parcouru on continu notre exploration
+		      (if (NOT (MYMEMBER etatPrometteur etatsParcourus)) 
+				  (SETQ sol (recherche_opti etatPrometteur etatFinal (append etatsParcourus (list etatCourrant))))
+				)
+				(if (null sol) ;;;; Si la solution est nul, on essaye avec un autre successeur.
+					(setq etatPrometteur (choixEtat etatFinal (remove etatPrometteur (successeurs etatCourrant)) etatsParcourus))
+					(setq etatPrometteur NIL)
+				)
 		 )
     sol)
   )
- )
+)
  
