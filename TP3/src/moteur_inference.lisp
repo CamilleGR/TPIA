655cc51c8339dae7bc274ad2afc01c438b31846a
CamilleGR <gerinrozecamille@gmail.com>
2015-12-24 17:58:38 +0100

    Ajout de l'affichage des règles et de la fonction AjouterFait

--- TP3/src/moteur_inference.lisp
+++ TP3/src/moteur_inference.lisp
@@ -1,1 +1,22 @@
 (load "base_de_regles.lisp")
+
+(defparameter *BaseFaits* (list))
+
+#|
+    Definition des fonctions outils
+|#
+
+(defun afficheRegles()
+  (dolist (x *BaseRegles* NIL)
+    (format t "~%~%Nom de règle : ~a~%premisse : ~a~%Nouveau Faits : ~a" (caddr x) (cadr x) (car x))
+  )
+)
+
+(defun AjouterFait(fait)
+  (if (assoc (car fait) *BaseFaits*)
+    (setf (assoc (car fait) *BaseFaits*) (cdr fait))
+    (push fait *BaseFaits)
+  )
+)
+
+(afficheRegles)
