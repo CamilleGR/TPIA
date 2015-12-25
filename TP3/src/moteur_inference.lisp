226fc51775d9077ce3839c3500fceb306a336887
CamilleGR <gerinrozecamille@gmail.com>
2015-12-25 23:01:42 +0100

    Christmas commit 2.0 #VerificationDesConditions #Swag

--- TP3/src/moteur_inference.lisp
+++ TP3/src/moteur_inference.lisp
@@ -14,11 +14,10 @@
 
 (defun TESTREGLES()
   (dolist (x *BaseRegles* NIL)
-  (if (premisseRespecte? x)
-    (format t "~%Nom de règle : ~a~%premisse : ~a~%Nouveau Faits : ~a~%" (caddr x) (cadr x) (car x))
+    ;;(format t "~%Nom de règle : ~a~%premisse : ~a~%Nouveau Faits : ~a~%" (caddr x) (cadr x) (car x))
+      (premisseRespecte? x)
     )
   )
-)
 
 (defun ajouterFait(fait)
   (if (assoc (car fait) *BaseFaits*)
@@ -40,15 +39,54 @@
 
 (defun premisseRespecte?(regle)
   ;;;;(format t "(car (getPremisse regle)) = ~a~%" (car (car (getPremisse regle))))
-  (cond
-    ((EQUAL (caar (getPremisse regle)) 'EGALITE)
-        (format t "BASE DE FAIT = ~a~%REGLE = ~a~%~%" (caadr (car (getPremisse regle))) (caddr (car (getPremisse regle))))
-        (EQUAL (cadr (ASSOC (caadr (car (getPremisse regle))) *BaseFaits*)) (cadr (cadr (getPremisse regle))))
-      )
-    ((EQUAL (caar (getPremisse regle)) 'DEFINI) NIL)
-    ((EQUAL (caar (getPremisse regle)) 'COMPARAISON) NIL)
+  (let ((test T))
+    (dolist ( x (getPremisse regle) test)
+      (setq test (AND test (conditionRespecte? x)))
     )
+  )
 )
+
+
+(defun conditionRespecte?(condition)
+;;;;(format t "Condition = ~a~%Type =~a~%Valeurs = ~a~%"
+;;;;  condition (car condition) (cadr (cadr condition)))
+(cond
+  ((EQUAL (car condition) 'EGALITE)
+      (format t "~a == ~a = ~a~%~%" (cadr (ASSOC (car (cadr condition)) *BaseFaits*)) (cadr (cadr condition)) (EQUAL (cadr (ASSOC (car (cadr condition)) *BaseFaits*)) (cadr (cadr condition))))
+      (EQUAL (cadr (ASSOC (cadr condition) *BaseFaits*)) (cadr (cadr condition)))
+    )
+  ((EQUAL (car condition) 'DEFINI)
+    (format t "~a EST IL DEFINI ?? ~%" condition )
+    (if (ASSOC (car (cadr condition)) *BaseFaits*)
+        (progn
+          (format t "OUI~%~%")
+          T
+      )
+      NIL))
+  ((EQUAL (car condition) 'COMPARAISON)
+    (format t "COMPARAISON = ~a~%" condition)
+    NIL)
+  )
+)
+#|
+  ~ = Alt + N
+  | = Alt + Shift + L
+
+  Pour la comparaison :
+    - Si c'est un comparateur alors on le garde
+    - Si c'est un opérateur on le garde
+    - Si c'est un symbole on va chercher sa valeur dans la base de fait (il faut vérifier qu'il existe)
+    - Si c'est une liste appel récursif
+
+    Faire deux fonctions :
+      - Une fonction qui va vérifier la comparaison  "verifierComparaison"
+      - Une fonction qui va évaleur toutes les opérandes "evalOperande"
+|#
+
+
+
 (ajouterFait (list 'TYPEDEROCHE 'Micachiste))
 (ajouterFait (list 'TYPEDEPIOCHE 'Double))
+(ajouterFait (list 'VITESSENAIN 1))
 (TESTREGLES)
+(print *BaseFaits*)
